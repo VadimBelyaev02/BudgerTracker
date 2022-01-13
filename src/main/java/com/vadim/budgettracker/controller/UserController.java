29a2c1af2edd3e0d6356559310e4751fa048881d
= <=>
2022-01-13 23:38:40 +0300

    Updated

--- src/main/java/com/vadim/budgettracker/controller/UserController.java
+++ src/main/java/com/vadim/budgettracker/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.vadim.budgettracker.controller;
 
 import com.vadim.budgettracker.dto.UserDTO;
+import com.vadim.budgettracker.entity.enums.Role;
 import com.vadim.budgettracker.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,11 +22,34 @@ public class UserController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public UserDTO getUser(@PathVariable("id") Long id) {
-        //return userService.getById(id);
+    public List<UserDTO> getUser(@PathVariable("id") Long id) {
+        List<UserDTO> userDTOS = new ArrayList<>();
         UserDTO userDTO = new UserDTO();
-        userDTO.setNickname("IT WORKS!");
-        return userDTO;
+        userDTO.setNickname("fdg");
+        userDTO.setCreatedDate(LocalDate.now());
+        userDTO.setConfirmed(false);
+        userDTO.setRole(Role.USER);
+
+        UserDTO userDTO1 = new UserDTO();
+        UserDTO userDTO2 = new UserDTO();
+        UserDTO userDTO3 = new UserDTO();
+        UserDTO userDTO4 = new UserDTO();
+        UserDTO userDTO5 = new UserDTO();
+        UserDTO userDTO6 = new UserDTO();
+        UserDTO userDTO7 = new UserDTO();
+        UserDTO userDTO8 = new UserDTO();
+
+        userDTO4.setNickname("IT WORKS!");
+        userDTOS.add(userDTO);
+        userDTOS.add(userDTO1);
+        userDTOS.add(userDTO2);
+        userDTOS.add(userDTO3);
+        userDTOS.add(userDTO4);
+        userDTOS.add(userDTO5);
+        userDTOS.add(userDTO6);
+        userDTOS.add(userDTO7);
+        userDTOS.add(userDTO8);
+        return userDTOS;
     }
 
     @GetMapping
