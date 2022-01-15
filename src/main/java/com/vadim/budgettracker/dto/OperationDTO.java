be2834f3425cd44c25246f0717c456eaabdbc018
= <=>
2022-01-16 01:42:07 +0300

    Refactoring

--- src/main/java/com/vadim/budgettracker/dto/OperationDTO.java
+++ src/main/java/com/vadim/budgettracker/dto/OperationDTO.java
@@ -1,14 +1,20 @@
 package com.vadim.budgettracker.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
@@ -17,6 +23,8 @@ public class OperationDTO {
     private Long id;
 
     @NotNull
+    @JsonSerialize(using = ToStringSerializer.class)
+  //  @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate createdDate;
 
     @NotNull
@@ -25,6 +33,5 @@ public class OperationDTO {
     @NotNull
     private Long userId;
 
-    @NotEmpty
     private String categoryName;
 }
