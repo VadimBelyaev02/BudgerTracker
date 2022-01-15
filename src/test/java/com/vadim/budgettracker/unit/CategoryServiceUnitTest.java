c49b1d72c974ebaf355e45239bc6d35961a608f8
= <=>
2022-01-15 20:11:39 +0300

    Refactoring

--- src/test/java/com/vadim/budgettracker/unit/CategoryServiceUnitTest.java
+++ src/test/java/com/vadim/budgettracker/unit/CategoryServiceUnitTest.java
@@ -4,7 +4,8 @@ import com.vadim.budgettracker.dao.CategoryDAO;
 import com.vadim.budgettracker.dto.CategoryDTO;
 import com.vadim.budgettracker.dto.converter.CategoryConverter;
 import com.vadim.budgettracker.entity.Category;
-import com.vadim.budgettracker.service.CategoryService;
+import com.vadim.budgettracker.exception.AlreadyExistsException;
+import com.vadim.budgettracker.exception.NotFoundException;
 import com.vadim.budgettracker.service.impl.CategoryServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -13,15 +14,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @DisplayName("UserService test")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -49,32 +50,99 @@ public class CategoryServiceUnitTest {
 
         assertEquals(service.getById(id), categoryDTO);
 
-        verify(categoryDAO, Mockito.only()).findById(id);
-        verify(converter, Mockito.only()).convertToDTO(category);
+        verify(categoryDAO, only()).findById(id);
+        verify(converter, only()).convertToDTO(category);
     }
 
+    @Test
+    public void Given_ServiceTriesToFindAllCategories_When_GetNothing_Then_ReturnResultList() {
+        List<Category> categories = new ArrayList<>();
+        List<CategoryDTO> categoryDTOS = new ArrayList<>();
+        Category category = new Category();
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categories.add(new Category());
+        categories.add(new Category());
+        categories.add(new Category());
+        categoryDTOS.add(new CategoryDTO());
+        categoryDTOS.add(new CategoryDTO());
+        categoryDTOS.add(new CategoryDTO());
 
+        when(categoryDAO.findAll()).thenReturn(categories);
+        when(converter.convertToDTO(category)).thenReturn(categoryDTO);
 
-    /*
+        assertEquals(service.getAll(), categoryDTOS);
 
+        verify(categoryDAO, only()).findAll();
+        verify(converter, times(3)).convertToDTO(category);
+    }
 
-       @Test
-    public void Given_ServiceTriesToFindUser_When_GetUserId_Then_FoundUserIsReturned() {
+    @Test
+    public void Given_ServiceTriesToSaveCategory_When_GetCategoryDTO_Then_ReturnSavedCategory() {
+        String name = "justName";
+        Category category = new Category();
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setName(name);
+
+        when(categoryDAO.existsByName(name)).thenReturn(false);
+        when(converter.convertToEntity(categoryDTO)).thenReturn(category);
+        when(converter.convertToDTO(category)).thenReturn(categoryDTO);
+        when(categoryDAO.save(category)).thenReturn(category);
+
+        assertEquals(service.save(categoryDTO), categoryDTO);
+
+        verify(categoryDAO, times(1)).existsByName(name);
+        verify(categoryDAO, times(1)).save(category);
+        verify(converter, times(1)).convertToDTO(category);
+        verify(converter, times(1)).convertToEntity(categoryDTO);
+    }
+
+    @Test
+    public void Given_ServiceTriesToSaveCategory_When_CategoryIsNotFound_Then_ThrowsException() {
+        String name = "justName";
+        Category category = new Category();
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setName(name);
+
+        when(categoryDAO.existsByName(name)).thenReturn(true);
+
+        assertThrows(AlreadyExistsException.class, () -> service.save(categoryDTO));
+
+        verify(categoryDAO, only()).existsByName(name);
+        verify(categoryDAO, never()).save(category);
+        verify(converter, never()).convertToDTO(category);
+        verify(converter, never()).convertToEntity(categoryDTO);
+    }
+
+    @Test
+    public void Given_ServiceTriesToUpdateCategory_When_GetCategory_Then_ReturnUpdatedCategory() {
         Long id = 1L;
-        String email = "vadimbelaev002@gmail.com";
-        User user = new User();
-        UserDTO userDTO = new UserDTO();
-        user.setId(id);
-        user.setEmail(email);
+        Category category = new Category();
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setId(id);
 
-        Mockito.when(userDAO.findById(id)).thenReturn(Optional.of(user));
+        when(categoryDAO.existsById(id)).thenReturn(true);
+        when(converter.convertToEntity(categoryDTO)).thenReturn(category);
+        when(converter.convertToDTO(category)).thenReturn(categoryDTO);
+        when(categoryDAO.update(category)).thenReturn(category);
 
-        Mockito.when(userConverter.convertToDTO(user)).thenReturn(userDTO);
+        assertEquals(service.update(categoryDTO), categoryDTO);
 
-        assertEquals(userService.getById(id), userDTO);
-
-        Mockito.verify(userDAO, Mockito.only()).findById(id);
-        Mockito.verify(userConverter, Mockito.only()).convertToDTO(user);
+        verify(categoryDAO, times(1)).existsById(id);
+        verify(categoryDAO, times(1)).update(category);
+        verify(converter, times(1)).convertToDTO(category);
+        verify(converter, times(1)).convertToEntity(categoryDTO);
     }
-     */
+
+    @Test
+    public void Given_ServiceTriesToDeleteCategory_When_NotFound_Then_ThrowException() {
+        Long id = 1L;
+
+        when(categoryDAO.existsById(id)).thenReturn(false);
+
+        assertThrows(NotFoundException.class, () -> service.deleteById(id));
+
+        verify(categoryDAO, only()).existsById(id);
+        verify(categoryDAO, never()).deleteById(id);
+    }
+
 }
