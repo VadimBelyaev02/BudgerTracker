1a21c86a7551c972cda0f570c1334a33fe40ffec
= <=>
2022-01-16 01:42:07 +0300

    Refactoring

--- src/main/java/com/vadim/budgettracker/dao/OperationRepository.java
+++ src/main/java/com/vadim/budgettracker/dao/OperationRepository.java
@@ -2,10 +2,8 @@ package com.vadim.budgettracker.dao;
 
 import com.vadim.budgettracker.entity.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OperationRepository extends JpaRepository<Operation, Long> {
 
