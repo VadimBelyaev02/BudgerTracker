2c1b01db0ec9cc1050e004bf0c312d7f1094d696
= <=>
2022-01-16 01:39:19 +0300

    Refactoring

--- src/main/java/com/vadim/budgettracker/exception/VkSendingException.java
+++
@@ -1,8 +0,0 @@
-package com.vadim.budgettracker.exception;
-
-public class VkSendingException extends RuntimeException {
-
-    public VkSendingException(String message) {
-        super(message);
-    }
-}
