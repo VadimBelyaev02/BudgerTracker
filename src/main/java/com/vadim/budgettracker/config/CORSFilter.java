d6c7fa84bfb5fd725d74396499a2556ee5468e75
= <=>
2022-01-17 01:03:12 +0300

    Refactoring

--- src/main/java/com/vadim/budgettracker/config/CORSFilter.java
+++ src/main/java/com/vadim/budgettracker/config/CORSFilter.java
@@ -16,15 +16,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With");
         chain.doFilter(req, res);
-    }
-
-    public void init(FilterConfig filterConfig) {
-
-    }
-
-    public void destroy() {
-
     }
 }
