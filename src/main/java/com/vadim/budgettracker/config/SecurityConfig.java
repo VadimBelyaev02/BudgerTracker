9a4d71d211b1eb108b0bcde3f2c9e14247d39934
= <=>
2022-01-27 20:45:43 +0300

    Refactoring

--- src/main/java/com/vadim/budgettracker/config/SecurityConfig.java
+++ src/main/java/com/vadim/budgettracker/config/SecurityConfig.java
@@ -83,7 +83,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
         configuration.setAllowCredentials(true);
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setMaxAge(3600L);
+        configuration.setExposedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "Origin"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
