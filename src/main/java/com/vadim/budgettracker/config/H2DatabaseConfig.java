4d8e54bc115b9ce0d04203d93fb95f7fef2ac11d
= <=>
2022-01-27 19:39:08 +0300

    Refactoring

--- src/main/java/com/vadim/budgettracker/config/H2DatabaseConfig.java
+++ src/main/java/com/vadim/budgettracker/config/H2DatabaseConfig.java
@@ -12,54 +12,54 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
-@Configuration
-@PropertySource("classpath:properties/db.properties")
-public class H2DatabaseConfig {
-
-    private final Environment environment;
-
-    public H2DatabaseConfig(Environment environment) {
-        this.environment = environment;
-    }
-
-//    @Bean(name = "h2DataSource")
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName(environment.getRequiredProperty("db.h2.DriverClassName"));
-//        dataSource.setUsername(environment.getRequiredProperty("db.h2.username"));
-//        dataSource.setPassword(environment.getRequiredProperty("db.h2.password"));
-//        dataSource.setUrl(environment.getRequiredProperty("db.h2.url"));
-//        return dataSource;
-//    }
-
-//    @Bean
-//    public DataSource dataSource(
-//            @Value("${datasource.dbname}") String dbname,
-//            @Value("${datasource.script}") String script) {
+//@Configuration
+//@PropertySource("classpath:properties/db.properties")
+//public class H2DatabaseConfig {
 //
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .setName(dbname)
-//                .addScript(script)
-//                .build();
+//    private final Environment environment;
+//
+//    public H2DatabaseConfig(Environment environment) {
+//        this.environment = environment;
 //    }
-
-//    @Bean(name = "h2TestDataSource")
-//    public DataSource dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .generateUniqueName(false)
-//                .setName("testdb")
-//                .setType(EmbeddedDatabaseType.H2)
-//                .addScript("classpath:sql/create-db.sql")
-//                .addScript("classpath:sql/create-users.sql")
-//                .addScript("classpath:sql/create-categories.sql")
-//                .addScript("classpath:sql/create-confirmations.sql")
-//                .addScript("classpath:sql/create-operations.sql")
-//                .setScriptEncoding("UTF-8")
-//                .ignoreFailedDrops(true)
-//                .build();
-//    }
-
-
-
-}
+//
+////    @Bean(name = "h2DataSource")
+////    public DataSource dataSource() {
+////        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+////        dataSource.setDriverClassName(environment.getRequiredProperty("db.h2.DriverClassName"));
+////        dataSource.setUsername(environment.getRequiredProperty("db.h2.username"));
+////        dataSource.setPassword(environment.getRequiredProperty("db.h2.password"));
+////        dataSource.setUrl(environment.getRequiredProperty("db.h2.url"));
+////        return dataSource;
+////    }
+//
+////    @Bean
+////    public DataSource dataSource(
+////            @Value("${datasource.dbname}") String dbname,
+////            @Value("${datasource.script}") String script) {
+////
+////        return new EmbeddedDatabaseBuilder()
+////                .setType(EmbeddedDatabaseType.H2)
+////                .setName(dbname)
+////                .addScript(script)
+////                .build();
+////    }
+//
+////    @Bean(name = "h2TestDataSource")
+////    public DataSource dataSource() {
+////        return new EmbeddedDatabaseBuilder()
+////                .generateUniqueName(false)
+////                .setName("testdb")
+////                .setType(EmbeddedDatabaseType.H2)
+////                .addScript("classpath:sql/create-db.sql")
+////                .addScript("classpath:sql/create-users.sql")
+////                .addScript("classpath:sql/create-categories.sql")
+////                .addScript("classpath:sql/create-confirmations.sql")
+////                .addScript("classpath:sql/create-operations.sql")
+////                .setScriptEncoding("UTF-8")
+////                .ignoreFailedDrops(true)
+////                .build();
+////    }
+//
+//
+//
+//}
---
+++ src/main/java/com/vadim/budgettracker/config/WebAppConfig2.java
@@ -0,0 +1,17 @@
+package com.vadim.budgettracker.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+
+//@Configuration
+//public class WebAppConfig2 implements WebApplicationInitializer {
+//    @Override
+//    public void onStartup(ServletContext servletContext) {
+//        servletContext
+//                .addServlet("H2Console", WebServlet.class)
+//                .addMapping("/console/*");
+//    }
+//}
