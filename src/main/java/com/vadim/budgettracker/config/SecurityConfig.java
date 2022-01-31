d95a63df29fd1972f9e6c7b70c9a4ecc29b97828
= <=>
2022-01-31 20:59:40 +0300

    Refactoring

--- src/main/java/com/vadim/budgettracker/config/SecurityConfig.java
+++ src/main/java/com/vadim/budgettracker/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http//.cors().and()
                 //.cors().disable()
                //  .cors().configurationSource(corsConfigurationSource()).and()
                 .csrf().disable()
