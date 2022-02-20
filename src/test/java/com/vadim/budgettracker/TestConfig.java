45593acb90c80a2d97534f4b6ca266ddfcd726cf
= <=>
2022-02-20 23:58:32 +0300

    Refactoring

--- src/test/java/com/vadim/budgettracker/TestConfig.java
+++ src/test/java/com/vadim/budgettracker/TestConfig.java
@@ -1,16 +1,12 @@
 package com.vadim.budgettracker;
 
 import com.vadim.budgettracker.config.ApplicationConfig;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -24,44 +20,52 @@ import java.util.Properties;
 @Configuration
 @ComponentScan(basePackages = "com.vadim.budgettracker")
 @TestPropertySource("src/test/resources/properties/db.properties")
-public class TestConfig extends ApplicationConfig {
+public class TestConfig {
 
     private final Environment environment;
 
     public TestConfig(Environment environment) {
-        super(environment);
         this.environment = environment;
     }
 
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("db.driverClassName"));
+        dataSource.setUsername(environment.getRequiredProperty("db.username"));
+        dataSource.setPassword(environment.getRequiredProperty("db.password"));
+        dataSource.setUrl(environment.getRequiredProperty("db.url"));
+        return dataSource;
+    }
 
-//    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-//        factory.setDataSource(dataSource());
-//        factory.setJpaVendorAdapter(hibernateJpaVendorAdapter());
-//        factory.setPackagesToScan(environment.getRequiredProperty("packagesToScan"));
-//        Properties props = new Properties();
-//        props.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-//      //  props.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-//        props.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
-//        factory.setJpaProperties(props);
-//        return factory;
-//    }
-//
-//    @Bean
-//    public JpaTransactionManager transactionManager() {
-//        JpaTransactionManager transactionManager = new JpaTransactionManager();
-//        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-//        return transactionManager;
-//    }
-//
-//    @Bean
-//    public HibernateJpaVendorAdapter hibernateJpaVendorAdapter() {
-//        return new HibernateJpaVendorAdapter();
-//    }
-//
-//    @Bean("entityManager")
-//    public EntityManager entityManager() {
-//        return Objects.requireNonNull(entityManagerFactory().getObject()).createEntityManager();
-//    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setJpaVendorAdapter(hibernateJpaVendorAdapter());
+        factory.setPackagesToScan(environment.getRequiredProperty("packagesToScan"));
+        Properties props = new Properties();
+        props.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        props.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        props.put("hibernate.ddl-auto", environment.getRequiredProperty("hibernate.ddl-auto"));
+        factory.setJpaProperties(props);
+        return factory;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    public HibernateJpaVendorAdapter hibernateJpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter();
+    }
+
+    @Bean("entityManager")
+    public EntityManager entityManager() {
+        return Objects.requireNonNull(entityManagerFactory().getObject()).createEntityManager();
+    }
 }
